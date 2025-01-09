c0158a5605c32056d747614134460a9a053db471
fernando <fernando100@windowslive.com>
2025-01-09 20:51:40 -0300

    09/01/2025 as 17:51

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // URL do backend
+  //baseURL: 'http://127.0.0.1:8000', // URL do backend
+  baseURL: 'https://fernando10092.pythonanywhere.com/',
 });
 
 export const getData = async () => {
