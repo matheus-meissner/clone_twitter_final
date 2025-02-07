8774f2339ceaec56597ed8509fe229f7a9dc6164
Matheus Meissner <matheus.iembo@gmail.com>
2025-02-07 15:49:11 -0300

    update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/fernando10092/Project--Twitter/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
