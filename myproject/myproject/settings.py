f78ebdbc992e215f380df9886ab6e87d3e4b3dcd
Matheus Meissner <matheus.iembo@gmail.com>
2025-02-07 15:57:25 -0300

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = "django-insecure-^*m080o&qsl7qj&z$@soua8d7scv$ao@d89endy)nx4-64^m#^
 DEBUG = True
 
 #não é seguro deixar com este *
-ALLOWED_HOSTS = ['fernando10092.pythonanywhere.com','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['matheusmeissnerebac.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
